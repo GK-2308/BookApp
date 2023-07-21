bd68ef9a7da9cc79f2d593ea8c4720f27bf9b5dc
GK-2308 <geetanshkhurana2@gmail.com>
2023-07-21 10:16:12 +0530

    Category

--- app/src/main/java/com/example/myapplication/activities/ProfileActivity.java
+++ app/src/main/java/com/example/myapplication/activities/ProfileActivity.java
@@ -1,11 +1,15 @@
 package com.example.myapplication.activities;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.example.myapplication.MyApplication;
@@ -13,7 +17,10 @@ import com.example.myapplication.R;
 import com.example.myapplication.adapters.AdapterPdfFavorite;
 import com.example.myapplication.databinding.ActivityProfileBinding;
 import com.example.myapplication.models.ModelPdf;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -26,15 +33,32 @@ public class ProfileActivity extends AppCompatActivity {
 
     private ActivityProfileBinding binding;
     private FirebaseAuth firebaseAuth;
+
+    private FirebaseUser firebaseUser;
     private ArrayList<ModelPdf> pdfArrayList;
     private AdapterPdfFavorite adapterPdfFavorite;
+
+    private ProgressDialog progressDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityProfileBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        binding.accountTypeTv.setText("N/A");
+        binding.memberDatetv.setText("N/A");
+        binding.favoriteBookCountTv.setText("N/A");
+        binding.accountStatusTv.setText("N/A");
+
         firebaseAuth = FirebaseAuth.getInstance();
+        // get current user
+        firebaseUser=firebaseAuth.getCurrentUser();
+
+        progressDialog=new ProgressDialog(this);
+        progressDialog.setTitle("Please wait");
+        progressDialog.setCanceledOnTouchOutside(false);
+
+        
         loadUserInfo();
         loadFavoriteBooks();
 
@@ -52,11 +76,73 @@ public class ProfileActivity extends AppCompatActivity {
                 onBackPressed();
             }
         });
+        
+        binding.accountStatusTv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(firebaseUser.isEmailVerified()){
+                    Toast.makeText(ProfileActivity.this, "Already verified...", Toast.LENGTH_SHORT).show();
+                }
+
+                else{
+                    emailVerifiedDialog();
+                }
+            }
+        });
+
+    }
+
+    private void emailVerifiedDialog() {
+        AlertDialog.Builder builder=new AlertDialog.Builder(this);
+        builder.setTitle("Verify Email")
+                .setMessage("Are you sure you want to send email verification instructions to your email "+firebaseUser.getEmail())
+                .setPositiveButton("SEND", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        sendEmailVerification();
+                    }
+                })
+                .setNegativeButton("CANCEl", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                })
+                .show();
+    }
+
+    private void sendEmailVerification() {
+        progressDialog.setMessage("sending email verification instruction to your email "+ firebaseUser.getEmail());
+        progressDialog.show();
+
+        firebaseUser.sendEmailVerification()
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+                        progressDialog.dismiss();
+                        Toast.makeText(ProfileActivity.this, "Instructions sent,check your email "+firebaseUser.getEmail(), Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        progressDialog.dismiss();
+                        Toast.makeText(ProfileActivity.this, "Failed due to "+e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
 
     }
 
     private void loadUserInfo()
     {
+
+        if(firebaseUser.isEmailVerified()){
+            binding.accountStatusTv.setText("Verified");
+        }
+        else{
+            binding.accountStatusTv.setText("not Verified");
+        }
+
         DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Users");
         reference.child(firebaseAuth.getUid())
                 .addValueEventListener(new ValueEventListener() {
